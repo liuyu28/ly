7b0c20a19ba8584354f52a6572cf7200d0c65b30
liuyu28 <1006216705@qq.com>
2017-09-17 17:08:17 +0800

    test

---
+++ ly/src/Lytest.java
@@ -0,0 +1,15 @@
+
+public class Lytest {
+	private  String str = "Hello";
+	private  char a[]={'a','b','c'};
+public static void main(String[] args) {
+	Lytest ly = new Lytest();
+	change(ly.str,ly.a);
+	System.out.println(ly.str);
+	System.out.println(ly.a);
+}
+public static void change(String str,char a[]){
+	str="change";
+	a[0]='g';
+}
+}
